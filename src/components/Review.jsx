a345ff46c021eb1c3d52bdf5fcf16c5a91ac6962
Pablo <parkour.pablo458@gmail.com>
2025-02-05 23:18:56 -0300

    add sliders

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import ReviewCard from "../layouts/ReviewCard";
-import img1 from "../assets/img/pic1.png";
-import img2 from "../assets/img/pic2.png";
-import img3 from "../assets/img/pic3.png";
+
 
 const Review = () => {
   return (
@@ -12,9 +10,7 @@ const Review = () => {
       </h1>
 
       <div className=" flex flex-col lg:flex-row gap-5 justify-center py-4 my-8">
-        <ReviewCard img={img1} title="Olivia Ava" />
-        <ReviewCard img={img2} title="John Deo" />
-        <ReviewCard img={img3} title="Sofia Zoe" />
+        <ReviewCard />
       </div>
     </div>
   );
--- src/layouts/ReviewCard.jsx
+++ src/layouts/ReviewCard.jsx
@@ -1,35 +1,109 @@
-import React from "react";
-import { FaQuoteRight } from "react-icons/fa";
-import { BsStarHalf } from "react-icons/bs";
-import { BsStarFill } from "react-icons/bs";
+// import React from "react";
+// import { FaQuoteRight } from "react-icons/fa";
+// import { BsStarHalf } from "react-icons/bs";
+// import { BsStarFill } from "react-icons/bs";
 
-const ReviewCard = (props) => {
+// const ReviewCard = (props) => {
+//   return (
+//     <div className=" flex flex-col w-full lg:w-2/6 bg-white p-3 rounded-lg gap-5">
+//       <div className=" flex flex-row items-center lg:justify-start justify-center">
+//         <div className="w-1/4">
+//           <img className=" rounded-full" src={props.img} alt="img" />
+//         </div>
+//         <div className=" mx-3">
+//           <h2 className=" font-semibold text-lg">{props.title}</h2>
+//           <div className=" flex">
+//             <BsStarFill className=" text-brightColor" />
+//             <BsStarFill className=" text-brightColor" />
+//             <BsStarFill className=" text-brightColor" />
+//             <BsStarFill className=" text-brightColor" />
+//             <BsStarHalf className=" text-brightColor" />
+//           </div>
+//         </div>
+//         <span className=" ml-16">
+//           <FaQuoteRight className=" text-backgroundColor" size={42} />
+//         </span>
+//       </div>
+//       <p>
+//         {props.text}
+//       </p>
+//     </div>
+//   );
+// };
+
+// export default ReviewCard;
+
+
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { FaQuoteRight } from "react-icons/fa";
+import { BsStarHalf, BsStarFill } from "react-icons/bs";
+import img1 from "../assets/img/pic1.png";
+import img2 from "../assets/img/pic2.png";
+import img3 from "../assets/img/pic3.png";
+
+const reviews = [
+  {
+    id: 1,
+    img: img1,
+    title: "João Silva",
+    text: "Ótima experiência, atendimento excelente!",
+  },
+  {
+    id: 2,
+    img: img2,
+    title: "Maria Souza",
+    text: "Adorei o serviço, super recomendo!",
+  },
+  {
+    id: 3,
+    img: img3,
+    title: "Carlos Lima",
+    text: "Serviço impecável e entrega rápida!",
+  },
+];
+
+const ReviewCard = () => {
   return (
-    <div className=" flex flex-col w-full lg:w-2/6 bg-white p-3 rounded-lg gap-5">
-      <div className=" flex flex-row items-center lg:justify-start justify-center">
-        <div className="w-1/4">
-          <img className=" rounded-full" src={props.img} alt="img" />
-        </div>
-        <div className=" mx-3">
-          <h2 className=" font-semibold text-lg">{props.title}</h2>
-          <div className=" flex">
-            <BsStarFill className=" text-brightColor" />
-            <BsStarFill className=" text-brightColor" />
-            <BsStarFill className=" text-brightColor" />
-            <BsStarFill className=" text-brightColor" />
-            <BsStarHalf className=" text-brightColor" />
+    <Swiper
+      modules={[Navigation, Pagination, Autoplay]}
+      spaceBetween={30}
+      slidesPerView={1}
+      navigation={false}
+      pagination={{ clickable: true }}
+      autoplay={{ delay: 3000 }}
+      className="w-full lg:w-2/6"
+    >
+      {reviews.map((review) => (
+        <SwiperSlide key={review.id}>
+          <div className="flex flex-col bg-white p-5 rounded-lg shadow-md gap-5">
+            <div className="flex flex-row items-center">
+              <div className="w-1/4">
+                <img className="rounded-full" src={review.img} alt="img" />
+              </div>
+              <div className="mx-3">
+                <h2 className="font-semibold text-lg">{review.title}</h2>
+                <div className="flex">
+                  <BsStarFill className="text-yellow-500" />
+                  <BsStarFill className="text-yellow-500" />
+                  <BsStarFill className="text-yellow-500" />
+                  <BsStarFill className="text-yellow-500" />
+                  <BsStarHalf className="text-yellow-500" />
+                </div>
+              </div>
+              <span className="ml-auto">
+                <FaQuoteRight className="text-gray-400" size={42} />
+              </span>
+            </div>
+            <p className="text-gray-700">{review.text}</p>
           </div>
-        </div>
-        <span className=" ml-16">
-          <FaQuoteRight className=" text-backgroundColor" size={42} />
-        </span>
-      </div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
-        consequatur fuga suscipit perspiciatis totam. Eos esse nisi omnis dicta,
-        aperiam distinctio fuga ipsam rerum rem.
-      </p>
-    </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 };
 
