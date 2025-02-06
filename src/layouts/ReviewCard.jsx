cb0f98154b731959a6a36f847a4979daea6b730d
Pablo <parkour.pablo458@gmail.com>
2025-02-05 23:32:58 -0300

    slides

--- src/layouts/ReviewCard.jsx
+++ src/layouts/ReviewCard.jsx
@@ -34,6 +34,81 @@
 // export default ReviewCard;
 
 
+// import React from "react";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import { Navigation, Pagination, Autoplay } from "swiper/modules";
+// import "swiper/css";
+// import "swiper/css/navigation";
+// import "swiper/css/pagination";
+// import { FaQuoteRight } from "react-icons/fa";
+// import { BsStarHalf, BsStarFill } from "react-icons/bs";
+// import img1 from "../assets/img/pic1.png";
+// import img2 from "../assets/img/pic2.png";
+// import img3 from "../assets/img/pic3.png";
+
+// const reviews = [
+//   {
+//     id: 1,
+//     img: img1,
+//     title: "João Silva",
+//     text: "Ótima experiência, atendimento excelente!",
+//   },
+//   {
+//     id: 2,
+//     img: img2,
+//     title: "Maria Souza",
+//     text: "Adorei o serviço, super recomendo!",
+//   },
+//   {
+//     id: 3,
+//     img: img3,
+//     title: "Carlos Lima",
+//     text: "Serviço impecável e entrega rápida!",
+//   },
+// ];
+
+// const ReviewCard = () => {
+//   return (
+//     <Swiper
+//       modules={[Navigation, Pagination, Autoplay]}
+//       spaceBetween={30}
+//       slidesPerView={1}
+//       navigation={false}
+//       pagination={{ clickable: true }}
+//       autoplay={{ delay: 3000 }}
+//       className="w-full lg:w-2/6"
+//     >
+//       {reviews.map((review) => (
+//         <SwiperSlide key={review.id}>
+//           <div className="flex flex-col bg-white p-5 rounded-lg shadow-md gap-5">
+//             <div className="flex flex-row items-center">
+//               <div className="w-1/4">
+//                 <img className="rounded-full" src={review.img} alt="img" />
+//               </div>
+//               <div className="mx-3">
+//                 <h2 className="font-semibold text-lg">{review.title}</h2>
+//                 <div className="flex">
+//                   <BsStarFill className="text-yellow-500" />
+//                   <BsStarFill className="text-yellow-500" />
+//                   <BsStarFill className="text-yellow-500" />
+//                   <BsStarFill className="text-yellow-500" />
+//                   <BsStarHalf className="text-yellow-500" />
+//                 </div>
+//               </div>
+//               <span className="ml-auto">
+//                 <FaQuoteRight className="text-gray-400" size={42} />
+//               </span>
+//             </div>
+//             <p className="text-gray-700">{review.text}</p>
+//           </div>
+//         </SwiperSlide>
+//       ))}
+//     </Swiper>
+//   );
+// };
+
+// export default ReviewCard;
+
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
@@ -45,6 +120,7 @@ import { BsStarHalf, BsStarFill } from "react-icons/bs";
 import img1 from "../assets/img/pic1.png";
 import img2 from "../assets/img/pic2.png";
 import img3 from "../assets/img/pic3.png";
+// import img4 from "../assets/img/pic4.png";
 
 const reviews = [
   {
@@ -65,6 +141,12 @@ const reviews = [
     title: "Carlos Lima",
     text: "Serviço impecável e entrega rápida!",
   },
+  // {
+  //   id: 4,
+  //   img: img4,
+  //   title: "Ana Pereira",
+  //   text: "Muito satisfeito com o atendimento!",
+  // },
 ];
 
 const ReviewCard = () => {
@@ -72,18 +154,23 @@ const ReviewCard = () => {
     <Swiper
       modules={[Navigation, Pagination, Autoplay]}
       spaceBetween={30}
-      slidesPerView={1}
+      slidesPerView={1} // Exibir 1 avaliação por slide em telas pequenas
+      breakpoints={{
+        768: {
+          slidesPerView: 2, // Exibir 2 avaliações por slide em telas a partir de 768px
+        },
+      }}
       navigation={false}
       pagination={{ clickable: true }}
       autoplay={{ delay: 3000 }}
-      className="w-full lg:w-2/6"
+      className="w-full lg:w-4/6"
     >
       {reviews.map((review) => (
         <SwiperSlide key={review.id}>
           <div className="flex flex-col bg-white p-5 rounded-lg shadow-md gap-5">
             <div className="flex flex-row items-center">
               <div className="w-1/4">
-                <img className="rounded-full" src={review.img} alt="img" />
+                <img className="w-full h-full rounded-full object-cover" src={review.img} alt={review.title} />
               </div>
               <div className="mx-3">
                 <h2 className="font-semibold text-lg">{review.title}</h2>
