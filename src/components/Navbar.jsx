bf4abd32bb1a8b36a19cbabcda49409c4b0cf9e2
Pablo <parkour.pablo458@gmail.com>
2025-02-05 21:56:45 -0300

    na div

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -87,7 +87,7 @@ const Navbar = () => {
             </Link>
           </nav>
 
-          <div className=" hidden lg:flex">
+          <div className=" hidden text-2xl lg:flex">
               <a href="https://api.whatsapp.com/send/?phone=5561992295015&text=Quero+iniciar+um+projeto%21&type=phone_number&app_absent=0 " target={"_blank"}><FaWhatsapp className="rounded-full text-2xl text-white bg-green-500"/></a>
           </div>
 
