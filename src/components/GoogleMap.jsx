5653c38c28934062f1809d5afb71e0112c1fe468
Pablo <parkour.pablo458@gmail.com>
2025-02-09 11:52:09 -0300

    da

--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 const GoogleMap = () => {
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex flex-col justify-center items-center lg:mb-10">
+        
       <iframe
         title="Google Map"
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d246114.8676405946!2d-47.183121894880294!3d-15.455100264918585!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9350e82d2198ca6d%3A0x8a0bda5ac04f74dc!2sBezerra%2C%20Formosa%20-%20GO!5e0!3m2!1spt-BR!2sbr!4v1739112280056!5m2!1spt-BR!2sbr"
@@ -14,6 +15,10 @@ const GoogleMap = () => {
         referrerPolicy="no-referrer-when-downgrade"
         className="rounded-lg shadow-lg"
       ></iframe>
+      <div className="mt-4 mb-4" >
+            <p>Bezerra-GO</p>
+            <p className="mt-3">Sem Taxa de Entrega!</p>
+        </div>
     </div>
   );
 };
